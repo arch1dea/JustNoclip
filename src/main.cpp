ea2a7a06ac57bde33064f12965ee9f48d39e5c58
arch1dea <lajkmiron@gmail.com>
2026-05-26 22:25:26 +0700

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,142 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayerObject.hpp>
+#include <Geode/binding/FLAlertLayer.hpp>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
+static bool noclipEnabled = false;
+static float rainbowHue = 0.f;
+static CCMenuItemSpriteExtra* button = nullptr;
+static CCLayer* menuLayer = nullptr;
+static bool isMenuOpen = false;
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+class $modify(PlayerObject) {
+    void deathState() {
+        if (!noclipEnabled) {
+            PlayerObject::deathState();
+        }
+    }
+};
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+class FloatingButton : public CCLayer {
+public:
+    static FloatingButton* create() {
+        auto ret = new FloatingButton();
+        if (ret && ret->init()) {
+            ret->autorelease();
+            return ret;
+        }
+        CC_SAFE_DELETE(ret);
+        return nullptr;
+    }
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
+    bool init() override {
+        auto winSize = CCDirector::get()->getWinSize();
+        
+        auto bg = CCSprite::create("square02b_001.png");
+        bg->setScaleX(3.5f);
+        bg->setScaleY(1.3f);
+        bg->setColor({0, 0, 0});
+        
+        auto outline = CCScale9Sprite::create("square02b_001.png");
+        outline->setScaleX(3.7f);
+        outline->setScaleY(1.5f);
+        outline->setZOrder(-1);
+        bg->addChild(outline);
+        outline->setPosition(bg->getContentSize() / 2);
+        
+        auto label = CCLabelBMFont::create("NO CLIP", "bigFont.fnt");
+        label->setScale(0.7f);
+        label->setPosition(bg->getContentSize() / 2);
+        bg->addChild(label);
+        
+        auto menu = CCMenu::create();
+        button = CCMenuItemSpriteExtra::create(bg, this, [](CCObject*) {
+            noclipEnabled = !noclipEnabled;
+            FLAlertLayer::create("NoClip", noclipEnabled ? "ON" : "OFF", "OK")->show();
+            auto bgSprite = static_cast<CCSprite*>(button->getChildByIndex(0));
+            if (bgSprite) {
+                bgSprite->setColor(noclipEnabled ? ccColor3B{40,40,40} : ccColor3B{0,0,0});
+            }
+        });
+        button->setPosition(winSize.width - 70, winSize.height - 50);
+        menu->addChild(button);
+        this->addChild(menu);
+        
+        this->schedule([this](float dt) {
+            if (!button) return;
+            rainbowHue += dt * 2.5f;
+            if (rainbowHue > 1.f) rainbowHue -= 1.f;
+            auto bg = static_cast<CCSprite*>(button->getChildByIndex(0));
+            if (!bg) return;
+            auto outline = static_cast<CCScale9Sprite*>(bg->getChildByIndex(0));
+            if (outline) {
+                auto color = geode::cocos::hsvToRgb({rainbowHue, 1.f, 1.f});
+                outline->setColor({color.r, color.g, color.b});
+            }
+        }, "rainbow", 0.03f, false);
+        
+        return true;
+    }
+};
 
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
+class NoClipMenu : public CCLayer {
+public:
+    static NoClipMenu* create() {
+        auto ret = new NoClipMenu();
+        if (ret && ret->init()) {
+            ret->autorelease();
+            return ret;
+        }
+        CC_SAFE_DELETE(ret);
+        return nullptr;
+    }
+    
+    bool init() override {
+        this->addChild(FloatingButton::create());
+        auto winSize = CCDirector::get()->getWinSize();
+        this->setPositionX(winSize.width);
+        return true;
+    }
+    
+    void openMenu() {
+        if (isMenuOpen) return;
+        isMenuOpen = true;
+        auto winSize = CCDirector::get()->getWinSize();
+        this->runAction(CCEaseBackOut::create(CCMoveTo::create(0.3f, ccp(0, 0))));
+    }
+    
+    void closeMenu() {
+        if (!isMenuOpen) return;
+        isMenuOpen = false;
+        auto winSize = CCDirector::get()->getWinSize();
+        this->runAction(CCEaseBackIn::create(CCMoveTo::create(0.3f, ccp(winSize.width, 0))));
+    }
+    
+    void toggleMenu() {
+        if (isMenuOpen) closeMenu();
+        else openMenu();
+    }
+};
 
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
+class $modify(MenuLayer) {
+    bool init() override {
+        if (!MenuLayer::init()) return false;
+        auto menu = NoClipMenu::create();
+        menu->setTag(9999);
+        this->addChild(menu);
+        menuLayer = menu;
+        return true;
+    }
+};
 
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
-
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+class $modify(CCKeyboardDispatcher) {
+    void dispatchKeyboard(cocos2d::enumKeyCodes key, bool down, bool arr) {
+        if (down && key == cocos2d::enumKeyCodes::KEY_J) {
+            if (menuLayer) {
+                static_cast<NoClipMenu*>(menuLayer)->toggleMenu();
+            }
+        }
+        CCKeyboardDispatcher::dispatchKeyboard(key, down, arr);
+    }
 };
